74b5f7dd510f363ea0525c62fef85f2e0470de6f
Joe Dettmar <joedettmar@gmail.com>
2017-10-18 18:57:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,3 @@ var message = `${name} is ${height} inches tall`
 console.log(message)
 
 module.exports = {name, height, message}
-
-var different = `I think ${name}'s Javascript is pretty weak.`
-
-console.log(different)
